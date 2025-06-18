511526bd08c0ee54f0419b2a51dfe1697e0c9f54
myferr <contactme.myfer@protonmail.com>
2025-06-18 02:41:36 -0500

    style(fonts): Add Berkeley Mono font and update code font styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./code-font.css";
+import "./proxima_vara.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
